d81b3a2a22a5995eccf1c03143b34308cf64b9a0
Denys Lonkovski Maiolli <denysmaiolli@hotmail.com>
2020-12-14 21:44:22 -0300

    add to-do, rm and rm all to-dos, save localstrg

---
+++ to-do-front/todos.js
@@ -0,0 +1,69 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+var buttonDelete = document.querySelector('#delete');
+
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
+
+function renderTodos() {
+    listElement.innerHTML = "";
+
+    for(todo of todos){
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#')
+        
+        //Remover todo procurando pelo texto
+        var pos = todos.indexOf(todo)
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos +')')
+
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.appendChild(linkText);
+        todoElement.appendChild(linkElement);
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
+
+function addTodo(){
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = "";
+    renderTodos();
+    saveToStorage();
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(pos){
+    todos.splice(pos, 1);
+    renderTodos();
+    saveToStorage();
+}
+
+function deleteAll(){
+    todos.splice(0, 500);
+    saveToStorage();
+    renderTodos();
+}
+
+buttonDelete.onclick = deleteAll;
+
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todos))
+}
+
+inputElement.addEventListener('keyup', function(e){
+  var key = e.which || e.keyCode;
+  if (key == 13) {
+    addTodo();
+    renderTodos();
+  }
+});
